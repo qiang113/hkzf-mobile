1b7c46cade1466681a1a8ef5f421e1e3833d025a
qiang113 <912391905@qq.com>
2019-08-01 22:49:10 +0800

    底部tab栏

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Citys from './components/Citys'
 import Map from './components/Map'
@@ -8,18 +8,6 @@ class App extends React.Component {
     return (
       <Router>
         <div>
-          APP组件
-          <ul>
-            <li>
-              <Link to="/home">首页</Link>
-            </li>
-            <li>
-              <Link to="/citys">首页</Link>
-            </li>
-            <li>
-              <Link to="/map">首页</Link>
-            </li>
-          </ul>
           <Route path="/home" component={Home} />
           <Route path="/citys" component={Citys} />
           <Route path="/map" component={Map} />
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,96 @@
 import React from 'react'
-import { Button } from 'antd-mobile'
+import { Route } from 'react-router-dom'
+import { TabBar } from 'antd-mobile'
+import Index from './Index/index.js'
+import House from './House'
+import News from './News'
+import My from './My'
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // 默认选中的Tab栏
+      selectedTab: 'redTab',
+      // 是否隐藏
+      hidden: false,
+      // 是否全屏
+      fullScreen: true
+    }
+  }
+
   render() {
     return (
       <div>
-        Home组件
-        <Button type="primary">primary</Button>
+        {/* 匹配规则 */}
+        <Route path="/home/index" component={Index} />
+        <Route path="/home/house" component={House} />
+        <Route path="/home/news" component={News} />
+        <Route path="/home/my" component={My} />
+
+        <div
+          style={
+            this.state.fullScreen
+              ? { position: 'fixed', height: '100%', width: '100%', top: 0 }
+              : { height: 400 }
+          }
+        >
+          <TabBar
+            unselectedTintColor="#949494"
+            tintColor="#33A3F4"
+            barTintColor="white"
+            hidden={this.state.hidden}
+          >
+            <TabBar.Item
+              title="首页"
+              key="首页"
+              icon={<i className="iconfont icon-ind" />}
+              selectedIcon={<i className="iconfont icon-ind" />}
+              selected={this.state.selectedTab === 'blueTab'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'blueTab'
+                })
+              }}
+            />
+            <TabBar.Item
+              icon={<i className="iconfont icon-findHouse" />}
+              selectedIcon={<i className="iconfont icon-findHouse" />}
+              title="找房"
+              key="找房"
+              selected={this.state.selectedTab === 'redTab'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'redTab'
+                })
+              }}
+            />
+            <TabBar.Item
+              icon={<i className="iconfont icon-infom" />}
+              selectedIcon={<i className="iconfont icon-infom" />}
+              title="资讯"
+              key="资讯"
+              selected={this.state.selectedTab === 'greenTab'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'greenTab'
+                })
+              }}
+            />
+            <TabBar.Item
+              icon={<i className="iconfont icon-my" />}
+              selectedIcon={<i className="iconfont icon-my" />}
+              title="我的"
+              key="我的"
+              selected={this.state.selectedTab === 'yellowTab'}
+              onPress={() => {
+                this.setState({
+                  selectedTab: 'yellowTab'
+                })
+              }}
+            />
+          </TabBar>
+        </div>
       </div>
     )
   }
